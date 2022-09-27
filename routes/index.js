5ce3a338b70f4933202d32c852c3d11441a2960b
Nat-Hunt <nathan.huntlds@gmail.com>
2022-09-26 19:28:37 -0500

    homepage working

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-// const { index } = require("../controllers/index");
+const { index } = require("../controllers/index");
 
 router.use('/contacts', require('./contacts'))
+router.use('/', index);
 
 module.exports = router;
