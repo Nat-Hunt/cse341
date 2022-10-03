b8a09aee2fc12285834ff1b2082fb50782cd9438
Nat-Hunt <nathan.huntlds@gmail.com>
2022-10-03 13:24:51 -0500

    Added POST, PUT, and DELETE routes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -26,4 +26,74 @@ const getOneDoc = async (req, res, next) => {
   });
 };
 
-module.exports = { getAllDocs, getOneDoc };
+const makeNewContact = async (req, res, next) => {
+  const newContact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+
+  const result = await mongodb
+    .getDb()
+    .db("CSE341")
+    .collection("contacts")
+    .insertOne(newContact);
+
+  if (result.acknowledged) {
+    res.status(201).json(result);
+  } else {
+    res
+      .status(500)
+      .json(result.error || "Some error occured while creating the contact.");
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  const queryid = new ObjectId(req.params.id);
+  const newValue = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const result = await mongodb
+    .getDb()
+    .db("CSE341")
+    .collection("contacts")
+    .replaceOne({ _id: queryid }, newValue);
+
+  if (result.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(result.error || "Some error occured while creating the contact.");
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  const queryid = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db("CSE341")
+    .collection("contacts")
+    .deleteOne({ _id: queryid }, true);
+  if (result.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(result.error || "Some error occured while creating the contact.");
+  }
+};
+
+module.exports = {
+  getAllDocs,
+  getOneDoc,
+  makeNewContact,
+  updateContact,
+  deleteContact,
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -5,4 +5,8 @@ const controller = require("../controllers/contacts");
 router.get("/", controller.getAllDocs);
 router.get("/:id", controller.getOneDoc);
 
+router.post("/", controller.makeNewContact);
+router.put("/:id", controller.updateContact);
+router.delete("/:id", controller.deleteContact);
+
 module.exports = router;
